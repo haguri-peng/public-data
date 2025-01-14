c541052338e067a400f716546240507e435dc907
haguri <hty851111@gmail.com>
2025-01-15 02:27:37 +0900

    불필요한 부분 삭제

--- src/test/java/com/haguri/publicdata/bus/controller/ExpBusControllerTest.java
+++ src/test/java/com/haguri/publicdata/bus/controller/ExpBusControllerTest.java
@@ -3,22 +3,14 @@ package com.haguri.publicdata.bus.controller;
 import com.haguri.publicdata.bus.service.ExpBusService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import reactor.core.publisher.Mono;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @WebMvcTest(ExpBusController.class)
 class ExpBusControllerTest {
@@ -32,14 +24,14 @@ class ExpBusControllerTest {
     @Test
     @DisplayName("고속버스 도시코드")
     void searchExpBusCityCode() throws Exception {
-//        Mono<ResponseEntity<String>> res = null;
-//        given(expBusService.getExpBusCityCode()).willReturn(res);
+        //Mono<ResponseEntity<String>> res = null;
+        //given(expBusService.getExpBusCityCode()).willReturn(res);
 
         mockMvc.perform(get("/api/expBus/cityCode"))
                 //.andDo(print())
                 .andExpect(status().isOk())
                 .andDo(print());
-                //.andExpect(content().string(containsString("서울특별시")));
+        //.andExpect(content().string(containsString("서울특별시")));
     }
 
     @Test
