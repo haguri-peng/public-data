3f50db445b1d650c15f3d702464041d6c3648f9c
haguri <hty851111@gmail.com>
2025-01-15 02:25:48 +0900

    json-simple -> gson

--- src/main/java/com/haguri/publicdata/util/JsonUtil.java
+++ src/main/java/com/haguri/publicdata/util/JsonUtil.java
@@ -1,9 +1,10 @@
 package com.haguri.publicdata.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
@@ -14,20 +15,20 @@ import java.util.Map;
 public class JsonUtil {
 
     /**
-     * JSONObject를 Map 형식으로 변환한다.
+     * JsonObject를 Map 형식으로 변환한다.
      *
-     * @param jsonObject Map으로 변환할 JSONObject
+     * @param jsonObject Map으로 변환할 JsonObject
      * @return Map
      */
     @SuppressWarnings("rawtypes")
-    public static Map getMapFromJSONObject(JSONObject jsonObject) {
+    public static Map getMapFromJsonObject(JsonObject jsonObject) {
         if (ObjectUtils.isEmpty(jsonObject)) {
             log.error("BAD REQUEST obj: {}", jsonObject);
             throw new IllegalArgumentException(String.format("BAD REQUEST OBJ %S", jsonObject));
         }
 
         try {
-            return new ObjectMapper().readValue(jsonObject.toJSONString(), Map.class);
+            return new ObjectMapper().readValue(jsonObject.getAsString(), Map.class);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -35,13 +36,13 @@ public class JsonUtil {
     }
 
     /**
-     * JSONArray를 List 형식으로 변환한다.
+     * JsonArray를 List 형식으로 변환한다.
      *
-     * @param jsonArray List로 변환할 JSONArray
+     * @param jsonArray List로 변환할 JsonArray
      * @return List
      */
     @SuppressWarnings("rawtypes")
-    public static List<Map> getListMapFromJSONArray(JSONArray jsonArray) {
+    public static List<Map> getListMapFromJSONArray(JsonArray jsonArray) {
         if (ObjectUtils.isEmpty(jsonArray)) {
             log.error("jsonArray is null.");
             throw new IllegalArgumentException("jsonArray is null");
@@ -49,37 +50,36 @@ public class JsonUtil {
 
         List<Map> list = new ArrayList<>();
         for (Object jsonObject : jsonArray) {
-            list.add(getMapFromJSONObject((JSONObject) jsonObject));
+            list.add(getMapFromJsonObject((JsonObject) jsonObject));
         }
         return list;
     }
 
     /**
-     * Map을 JSONObject로 변환한다.
+     * Map을 JsonObject로 변환한다.
      *
-     * @param map JSONObject로 변환할 Map
-     * @return JSONObject
+     * @param map JsonObject로 변환할 Map
+     * @return JsonObject
      */
-    @SuppressWarnings("unchecked")
-    public static JSONObject getJSONObjectFromMap(Map<String, Object> map) {
-        JSONObject jsonObject = new JSONObject();
+    public static JsonObject getJsonObjectFromMap(Map<String, Object> map) {
+        JsonObject jsonObject = new JsonObject();
         for (String key : map.keySet()) {
-            jsonObject.put(key, map.get(key));
+            jsonObject.add(key, (JsonElement) map.get(key));
         }
         return jsonObject;
     }
 
     /**
-     * List를 JSONArray로 변환한다.
+     * List를 JsonArray로 변환한다.
      *
-     * @param list JSONArray로 변환할 List
-     * @return JSONArray
+     * @param list JsonArray로 변환할 List
+     * @return JsonArray
      */
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public static JSONArray getJSONArrayFromList(List<Map> list) {
-        JSONArray jsonArray = new JSONArray();
+    public static JsonArray getJsonArrayFromList(List<Map> list) {
+        JsonArray jsonArray = new JsonArray();
         for (Map<String, Object> map : list) {
-            jsonArray.add(getJSONObjectFromMap(map));
+            jsonArray.add(getJsonObjectFromMap(map));
         }
         return jsonArray;
     }
